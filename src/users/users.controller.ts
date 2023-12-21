ec8fa9de4f7283f7cb6f24306cbf70a5d6504943
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-21 11:51:10 +0200

    refactor: refactor the controller to contain the route handler methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, NotFoundException } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const user = this.usersService.findOne(id);
+    if (!user) throw new NotFoundException('User does not exist!');
+    return user;
+  }
+
+  @Post()
+  async create(@Body() user: User) {
+    return this.usersService.create(user);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() user: User) {
+    return this.usersService.updateUser(user, id);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+}
