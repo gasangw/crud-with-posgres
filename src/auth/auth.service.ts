10723605e28ee8f48b519a1a50fc87e29013c176
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-01-08 12:43:56 +0200

    refactor: refactor the register method to include the salt and hash the password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,23 +8,28 @@ import { UsersService } from 'src/users/users.service';
 export class AuthService {
   constructor(private UserService: UsersService) {}
 
-  async hashedPassword(password: string): Promise<string> {
-    return await bcrypt.hash(password, 12);
-  }
+  // async hashedPassword(password: string): Promise<string> {
+  //   return await bcrypt.hash(password, 12);
+  // }
 
   async register(user: NewUserDto): Promise<User[] | any> {
     // check if the email is in use
     const existingUser = await this.UserService.findOneByEmail(user.email);
     if (existingUser) throw new BadRequestException('Email is in use');
     // hash the password
-    const hashedPassword = await this.hashedPassword(user.password);
+    // create a salt
+    const salt = await bcrypt.genSalt();
+    // hash the password with the salt
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    // join the hashed password and the salt together
+    const hashedPasswordWithSalt = `${hashedPassword}.${salt}`;
+    // save the password
+    // create the user and save them
     const newUser = await this.UserService.create({
       ...user,
-      password: hashedPassword,
+      password: hashedPasswordWithSalt,
     });
-    // create the user and save them
     return newUser;
-    // return the user
   }
 
   signin() {}
